7e38220b8b6a2321353d327547abfad90692070b
lorenzocalifano <info.lorenzocalifano@gmail.com>
2026-06-15 17:44:48 +0200

    Red Zones implementato con placeholders

--- app/src/main/java/com/lorenzocalifano/shieldup/MainActivity.kt
+++ app/src/main/java/com/lorenzocalifano/shieldup/MainActivity.kt
@@ -2,6 +2,8 @@ package com.lorenzocalifano.shieldup
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.lorenzocalifano.shieldup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,11 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/lorenzocalifano/shieldup/ui/redzones/RedZonesFragment.kt
+++ app/src/main/java/com/lorenzocalifano/shieldup/ui/redzones/RedZonesFragment.kt
@@ -1,6 +1,26 @@
 package com.lorenzocalifano.shieldup.ui.redzones
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.lorenzocalifano.shieldup.R
 
-class RedZonesFragment : Fragment(R.layout.fragment_red_zones)
+class RedZonesFragment : Fragment(R.layout.fragment_red_zones) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val addButton = view.findViewById<Button>(R.id.btnAddRedZone)
+        val listButton = view.findViewById<Button>(R.id.btnRedZoneList)
+
+        addButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Aggiunta Red Zone da implementare", Toast.LENGTH_SHORT).show()
+        }
+
+        listButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Lista Red Zones da implementare", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
